37524efe0ec5c7d3ca708be80d4c9f7a3d647015
Marta Pacuszka <pacuszka.m@gmail.com>
2020-09-02 13:06:11 +0200

    Improve insertion sort

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -2,10 +2,10 @@ from algorithms.sorting_algorithm import SortingAlgorithm
 
 class InsertionSort(SortingAlgorithm):
     def sort(self, arr: list) -> list:
-        for i, el in enumerate(arr):
-            j = i
-            while (j > 0 and arr[j] < arr[j-1]):
-                arr[j] = arr[j-1]
-                arr[j-1] = el
+        for i in range(1, len(arr)):
+            el = arr[i]
+            j = i - 1
+            while (j >= 0 and arr[j] > el):
+                arr[j + 1] = arr[j]
                 j -= 1
         return arr
